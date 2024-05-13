d6eefb5e8e6f09facb6de7380e9206db477e4f7e
Freewind <nowindlee@gmail.com>
2024-05-13 11:26:12 +0800

    typescript-window-scroll-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript HTML Demo
+TypeScript Window Scroll Demo
 =================================
+
+window.scroll可以指定top，即跳转后上面溢出的高度等于top
 
 ```
 npm i
--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,9 @@
-console.log('### Hello');
+const button = document.getElementById('button')!;
+const input = document.getElementById('input') as HTMLInputElement;
+
+button.addEventListener('click', () => {
+    console.log("### input.value", input.value)
+    window.scroll({
+        top: parseInt(input.value ?? 0)
+    })
+})
